7fecf172446969895333fa005f804f225cbaf9fa
AnotherDev <rafaelsilvahome@gmail.com>
2026-01-06 14:26:12 -0300

    added player class with draw() method but no movement

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
-
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x, y = SCREEN_WIDTH/2, SCREEN_HEIGHT/2
+    player = Player(x,y)
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -15,7 +19,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+        print(f"{dt}")
 
+        
+        
 if __name__ == "__main__":
     main()
