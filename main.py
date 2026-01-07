f83db229e2ab6a999ab9ed9d38940f98083bd4ab
AnotherDev <rafaelsilvahome@gmail.com>
2026-01-06 23:11:34 -0300

    added rotation to the player

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from player import Player
 def main():
     pygame.init()
     clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     x, y = SCREEN_WIDTH/2, SCREEN_HEIGHT/2
     player = Player(x,y)
@@ -19,12 +18,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        dt = clock.tick(60)/1000
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60)/1000
         print(f"{dt}")
 
-        
-        
+                
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -20,3 +20,15 @@ class Player(CircleShape):
     #Draws the player triangle body
     def draw(self, screen):
         return pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+            
